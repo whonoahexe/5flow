8b38b259a84f8baf21165e1313ea25e3eb94b2bd
akdeepankar <akdeepaknyc@gmail.com>
2025-09-21 23:26:40 +0530

    Added CTA Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 // import { Cta } from '@/components/layout';
 
+import { Cta } from "@/components/layout";
+
 export default function Home() {
   return (
     <>
-      {/* <Cta
+    <Cta
         leftTitle="Experience"
         leftSubtitle="What’s Next in"
         rightTitle="Artwork Management"
         rightDesc="Get a live demo of our advanced artwork management software by our product experts."
         buttonText="Book A Demo"
-      /> */}
+      />
     </>
   );
 }
