e0c0f8c8dc51ee7c260e0b7e334f39d121d9d7d9
5flow <sarah.hirzinger@propelis.com>
2025-12-22 17:33:22 +0530

    add paading

--- app/resources/blogs/[slug]/page.tsx
+++ app/resources/blogs/[slug]/page.tsx
@@ -27,7 +27,7 @@ export default async function BlogPost({ params }: { params: Promise<{ slug: str
       ) : null}
 
       <div className="container mx-auto mb-32">
-        <div className="mx-auto max-w-3xl py-10">
+        <div className="mx-auto max-w-3xl py-10 px-5 md:px-0">
           <h1 className="font-heading mb-6 text-3xl leading-tight tracking-tight sm:text-4xl">{post.title}</h1>
           <div className="text-foreground/60 mb-8 text-sm">{new Date(post.date).toLocaleDateString()}</div>
           <HtmlContent html={post.content} />
